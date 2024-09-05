5ddc2a44be6720432d4602c9e5ae9382f8856315
Salsabil <salsabeelahmed323@gmail.com>
2024-09-05 14:16:54 +0300

    try

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'])
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
 def unauthorized() -> None:
     """endpoint must raise a 401 error"""
     abort(401)

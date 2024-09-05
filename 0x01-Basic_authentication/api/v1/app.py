b35eff52be0f26a585ec837bf27e6c1c2ac3016c
Salsabil <salsabeelahmed323@gmail.com>
2024-09-05 16:37:38 +0300

    Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 import os
 
 
@@ -18,7 +19,9 @@ auth = None
 
 # Load the appropriate authentication type based on the environment variable
 auth_type = os.getenv('AUTH_TYPE')
-if auth_type == 'auth':
+if auth_type == "basic_auth":
+    auth = BasicAuth()
+else:
     auth = Auth()
 
 
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,39 @@
+# api/v1/auth/basic_auth.py
+import base64
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class that inherits from Auth.
+    """
+
+    def extract_base64_authorization_header(self, authorization_header: str
+                                            ) -> str:
+        """
+        Extract the Base64 part of the Authorization header.
+        """
+        if authorization_header is None or not isinstance(authorization_header,
+                                                          str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        # Return the part after "Basic "
+        return authorization_header.split(" ", 1)[1]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """
+        Decode the Base64 string to its original value.
+        """
+        if base64_authorization_header is None or not isinstance(
+            base64_authorization_header, str
+        ):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            # Decode as UTF-8 and return the string
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None

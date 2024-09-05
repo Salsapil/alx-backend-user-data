e4a805047e015b1866bf9211f33c405324beb8c9
Salsabil <salsabeelahmed323@gmail.com>
2024-09-05 14:36:51 +0300

    app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,9 +4,8 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, abort
 from flask_cors import (CORS, cross_origin)
-import os
 
 
 app = Flask(__name__)
@@ -16,15 +15,20 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized_error(error) -> str:
-    """error handler for 401 status code"""
+    """ Error handler for 401 status code """
     return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden_error(error) -> str:
+    """ Error handler for 403 status code """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-""" Module of Index views
-"""
+""" Module of Index views """
 from flask import jsonify, abort
 from api.v1.views import app_views
 
@@ -26,7 +25,13 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'])
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> None:
-    """endpoint must raise a 401 error"""
+    """ Endpoint to raise a 401 error """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> None:
+    """ Endpoint to raise a 403 error """
+    abort(403)

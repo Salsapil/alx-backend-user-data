438cb7b81a1c89d15aa0bbae0d0e6a1c9075979d
Salsabil <salsabeelahmed323@gmail.com>
2024-09-10 23:32:12 +0300

    auth.py

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Hash password"""
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """
+    Hash the input password using bcrypt, returning the salted hash as bytes.    
+    """
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    
+    return hashed_password
